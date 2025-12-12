203681be7f4e3ef6026413bb347a589e45668f27
Himanshu Yadav <imhimanshu1605@gmail.com>
2025-12-13 02:54:13 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api'
+  baseURL: import.meta.env.VITE_API_URL || 'https://mern-task-app-s9vo.onrender.com/api'
 })
 
 // attach token automatically
